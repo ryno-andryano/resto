3e153f5ec5687cde90103afccdcdfb0fb8ae9b05
Ryno Andryano <rynoandryano@gmail.com>
2022-08-20 19:21:16 +0700

    Applied Image Compression

--- webpack.common.js
+++ webpack.common.js
@@ -1,6 +1,9 @@
+/* eslint-disable new-cap */
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const ImageminWebpackPlugin = require('imagemin-webpack-plugin').default;
+const ImageminMozjpeg = require('imagemin-mozjpeg');
 const path = require('path');
 
 module.exports = {
@@ -41,6 +44,14 @@ module.exports = {
           from: path.resolve(__dirname, 'src/public/'),
           to: path.resolve(__dirname, 'dist/'),
         },
+      ],
+    }),
+    new ImageminWebpackPlugin({
+      plugins: [
+        ImageminMozjpeg({
+          quality: 50,
+          progressive: true,
+        }),
       ],
     }),
   ],
